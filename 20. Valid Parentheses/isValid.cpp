5092c097ac3c3de8b40acc267351d0d307bc1430
Sfsx <405909878@qq.com>
2019-06-11 11:16:04 +0800

    20. Valid Parentheses

---
+++ 20. Valid Parentheses/isValid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> s1;
+        s1.push('s');
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ')' && s1.top() == '(') {
+                s1.pop();
+            } else if (s[i] == '}' && s1.top() == '{') {
+                s1.pop();
+            } else if (s[i] == ']' && s1.top() == '[') {
+                s1.pop();
+            } else {
+                s1.push(s[i]);
+            }
+        }
+        return s1.top() == 's';
+    }
+    bool isValid(string s) {
+        stack<char> bs;
+        bs.push('s'); // base for stack so that bs.top() doesn't return NULL
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(' or s[i] == '{' or s[i] == '[') {
+                bs.push(s[i]); // push every valid parenthesis
+                continue;
+            }
+            if((s[i] == ')' and bs.top() == '(') or (s[i] == '}' and bs.top() == '{') or (s[i] == ']' and bs.top() == '[')) {
+                bs.pop(); //pop every open parenthesis for matching closed one
+            } else {
+                bs.push(s[i]); // push non-matching closed parenthesis
+            }
+        }
+        return bs.top() == 's'; // if all parenthesis matched correctly stack will contain only base
+    }
+};
